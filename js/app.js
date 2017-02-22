c2ae409848de62cec58ffd8f91c417ea15b2bebd
Swati Gupta <swati.gu11@gmail.com>
2017-02-22 15:22:17 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,21 +1,32 @@
 /**
-	
+	An app using HTML,CSS and OOJS to search facebook pages
+	using Facebook Graph API
 **/
-
+document.getElementById("searchBox").focus();
 var pageSearchApp = {
    loader  : document.getElementsByClassName("loader")[0],
 	/**
 		Invoked while pressing enter key on searchBox
 	**/
    onSearchFieldEnter : function(ev) {
-		var that         = this,
-			searchField  = document.getElementById("searchBox"),
-			apiUrl       = "https://graph.facebook.com/v2.8/search?q=",
-			access_token = "EAAaMGUNe4XUBAKrKd1wG1dO6gGF2pzMzQn7em0hDXTaZC6EDtZAKiwEXF8Ydfo5neePB9jEDknflZBEYyE95xAQaVEWqRpU3CZAkNTiiEhRuV4ixR7eF31166fQ6KgIluRB3CAwG5iDBIh4KojlNggCeJ5is0E4ZD";
+		var that           = this,
+			searchField    = document.getElementById("searchBox"),
+			pagesContainer = document.getElementById("pagesContainer"),
+			apiUrl         = "https://graph.facebook.com/v2.8/search?q=",
+			access_token   = "EAAaMGUNe4XUBAKrKd1wG1dO6gGF2pzMzQn7em0hDXTaZC6EDtZAKiwEXF8Ydfo5neePB9jEDknflZBEYyE95xAQaVEWqRpU3CZAkNTiiEhRuV4ixR7eF31166fQ6KgIluRB3CAwG5iDBIh4KojlNggCeJ5is0E4ZD";
 
-		apiUrl = apiUrl+searchField.value+"&type=page&limit=25&fields=id,name,about,category,company_overview,bio,picture,overall_star_rating&access_token="+access_token;
-		that.loaderUtil("block");
-		that.createAjaxRequest("GET",apiUrl,that.pageCallbackFunction);
+		if(searchField.value!=""){
+			pagesContainer.innerHTML = "";
+			apiUrl = apiUrl+searchField.value+"&type=page&limit=25&fields=id,name,about,category,company_overview,bio,picture,overall_star_rating&access_token="+access_token;
+			that.loaderUtil("block");
+			that.createAjaxRequest("GET",apiUrl,that.pageCallbackFunction);
+		}
+		else{
+			pagesContainer.innerHTML="<pre>\
+										<h1>Ahhh...! You forgot to enter the page!!!</h1>\
+										<h1>Please enter the page to be searched...</h1>\
+									</pre>";
+		}
 	},
 	/**
 		Invoke during clicking of the card
@@ -37,18 +48,23 @@ var pageSearchApp = {
 	      	var response = JSON.parse(xhrResponse.response).data,
 	      		dom      = "";
 	      		
-	      	response.forEach(function(resultData){
-	      		var card = "";
-		      	card+="<div class='card' pageId="+resultData.id+" onclick='pageSearchApp.onClickCard(this)'>";
-	      			card+="<img src='" + resultData.picture.data.url + "'>";
-	      			card+="<div class='card-body'>";
-	      				card+="<h4><b>"+resultData.name+"</b></h4>";
-	      				card+="<p>"+resultData.category+"</p>";
-	      			card+="</div>";
-		      	card+="</div>";
-	      		dom+=card;
-	      	});
-	      	pagesContainer.innerHTML = dom;
+	      	if(response.length){
+	      		response.forEach(function(resultData){
+		      		var card = "";
+			      	card+="<div class='card' pageId="+resultData.id+" onclick='pageSearchApp.onClickCard(this)'>";
+		      			card+="<img src='" + resultData.picture.data.url + "'>";
+		      			card+="<div class='card-body'>";
+		      				card+="<h4><b>"+resultData.name+"</b></h4>";
+		      				card+="<p>"+resultData.category+"</p>";
+		      			card+="</div>";
+			      	card+="</div>";
+		      		dom+=card;
+		      	});
+		      	pagesContainer.innerHTML = dom;
+	      	}
+	      	else{
+	      		pagesContainer.innerHTML = "<h1>Oops...No Pages Found!!!</h1>";
+	      	}
 	      	that.loaderUtil("none");
 	   }
 	},
